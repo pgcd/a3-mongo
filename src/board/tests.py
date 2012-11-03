fb8b559cefb2fabc21da6598a5fa1e48cef5e6e1
Paolo Dente <paolo.dente@prodigysgroup.it>
2012-11-03 13:06:55 +0100

    Added super-basic tests for the list.

--- src/board/tests.py
+++ src/board/tests.py
@@ -9,8 +9,26 @@ from django.test import TestCase
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        super(SimpleTest, self).setUp()
+
+    def test_homepage_topic_list(self):
+        r = self.client.get('/')
+        self.assertEqual(r.status_code,200)
+        self.assertTrue('topic_list' in r.context)
+
+    def test_topic_list(self):
+        r = self.client.get('/p/')
+        self.assertEqual(r.status_code,200)
+        self.assertTrue('topic_list' in r.context)
+
+    def test_post_new_topic(self):
+        r = self.client.post('/p/', {'title':'test topic', 'body_markup':'test body markup'}, follow=True)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.context['topic'].title, 'test topic')
+        self.assertEqual(r.context['topic'].body, 'test body markup')
+        self.assertEqual(r.context['topic'].obj.body, 'test body markup')
+        r = self.client.get('/p/')
+        self.assertEqual(r.status_code,200)
+        self.assertTrue(r.context['topic_list'])
+        self.assertGreaterEqual(len(r.context['topic_list']),1)
